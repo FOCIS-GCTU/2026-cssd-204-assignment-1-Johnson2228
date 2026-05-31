1530a12b4664f502e9fc8737dc0c8b12ea27e0d0
Johnson2228 <2425402228@live.gctu.edu.gh>
2026-05-31 19:03:12 +0100

    Update initials.py

--- initials.py
+++ initials.py
@@ -1,7 +1,33 @@
+# File: initials.py
+# Description: Print out my initials.
+# Assignment Number: 2
+#
+# Name: Johnson Agyemang
+# STUDENT ID: 2425402228
+# Email: 2425402228@live.gctu.edu.gh
+# Grader: Augustus Buckman
+#
+# On my honor, Augustus Buckman, this programming assignment is my own work
+# and I have not provided this code to any other student.
 
 def main():
-  pass #code goes here
+    # Prints my initials in large letters
 
+    print()
+    print("...JAA")
+    print()
 
+    print("......JJJJJJJJJ........AAAAAAAAAAAA........AAAAAAAAAAAA.....")
+    print(".........JJJJ..........AAAA....AAAA........AAAA....AAAA.....")
+    print(".........JJJJ..........AAAA....AAAA........AAAA....AAAA.....")
+    print(".........JJJJ..........AAAA....AAAA........AAAA....AAAA.....")
+    print(".........JJJJ..........AAAAAAAAAAAA........AAAAAAAAAAAA.....")
+    print(".........JJJJ..........AAAAAAAAAAAA........AAAAAAAAAAAA.....")
+    print(".........JJJJ..........AAAA....AAAA........AAAA....AAAA.....")
+    print(".........JJJJ..........AAAA....AAAA........AAAA....AAAA.....")
+    print("...JJJ...JJJJ.....**...AAAA....AAAA...**...AAAA....AAAA...**")
+    print("....JJJJJJJ.......**...AAAA....AAAA...**...AAAA....AAAA...**")
+
+    print()
 
 main()
